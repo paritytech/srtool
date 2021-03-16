0ee825b6d72557f5ab82d43804bc20e4e937fdb5
chevdor <chevdor@gmail.com>
2021-03-16 17:48:06 +0100

    Bump all versions and reduce noise in the build output

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ RUSTC_VERSION=`cat RUSTC_VERSION`
 REPO=chevdor/srtool
 
 echo Building $REPO:$RUSTC_VERSION
-echo Any arg you pass is forward to `docker build`... You can pass `--no-cache` for instance
+echo Any arg you pass is forward to 'docker build'... You can pass'`--no-cache' for instance
 
 docker build $@ --build-arg RUSTC_VERSION=$RUSTC_VERSION -t $REPO:$RUSTC_VERSION .
