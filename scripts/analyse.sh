16e58625f9d8ae9d62e95c04c87211ecd7af646e
Antonio Antonino <antonio@kilt.io>
2022-09-12 13:32:38 +0200

    Add parachain authorize upgrade proposal hash to output

--- scripts/analyse.sh
+++ scripts/analyse.sh
@@ -4,7 +4,8 @@ WASM=$1
 WASM_FULLPATH=/build/$WASM
 
 SZ=`du -sb $WASM_FULLPATH | awk '{print $1}'`
-PROP=`subwasm -j info $WASM_FULLPATH | jq -r .proposal_hash`
+SET_CODE_PROP=`subwasm -j info $Z_WASM | jq -r .proposal_hash`
+AUTHORIZE_UPGRADE_PROP=`subwasm -j info $Z_WASM | jq -r .parachain_authorize_upgrade_hash`
 MULTIHASH=`subwasm -j info $WASM_FULLPATH | jq -r .ipfs_hash`
 SHA256=0x`shasum -a 256 $WASM_FULLPATH | awk '{print $1}'`
 TMSP=$(date --utc +%FT%TZ -d @$(stat -c "%Y" $WASM_FULLPATH))
@@ -14,7 +15,8 @@ SUBWASM=`subwasm -j info $WASM_FULLPATH`
 JSON=$( jq -n \
     --arg tmsp "$TMSP" \
     --arg size "$SZ" \
-    --arg prop "$PROP" \
+    --arg set_code_prop "$SET_CODE_PROP" \
+    --arg authorize_upgrade_prop "$AUTHORIZE_UPGRADE_PROP" \
     --arg blake2_256 "$BLAKE2_256" \
     --arg ipfs "$MULTIHASH" \
     --arg sha256 "$SHA256" \
@@ -23,7 +25,8 @@ JSON=$( jq -n \
     '{
         tmsp: $tmsp,
         size: $size,
-        prop: $prop,
+        set_code_prop: $set_code_prop,
+        authorize_upgrade_prop: $authorize_upgrade_prop,
         blake2_256: $blake2_256,
         ipfs: $ipfs,
         sha256: $sha256,
